9037247b8bea2fc7bfa8cf85ed78cb7b78fdfb3f
sheetsvicky <sheeeets@gmail.com>
2016-08-02 10:09:40 +0800

    Create DiffSearch.java

---
+++ DiffSearch.java
@@ -0,0 +1,8 @@
+/* 2016.8.1, improve difference based algorithm 
+ * 1) read test and training set
+ * 2) calculate difference
+ * 3) search diff-seq in the database, if hit, predict the next integer; if not, continue 2)
+ */
+ class DiffSearch{
+ 
+ }
